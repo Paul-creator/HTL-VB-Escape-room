f6c70254909fadb9d7cb078ccd59549b00b20c60
Paul Kronegger <paulkroneggerdevelopment@gmx.at>
2020-10-22 17:11:43 +0200

    working

--- TagDerOffenenTuerEscapeGame/TagDerOffenenTuerEscapeGame/ExternalWebGame/ExternalWebGame.gd
+++ TagDerOffenenTuerEscapeGame/TagDerOffenenTuerEscapeGame/ExternalWebGame/ExternalWebGame.gd
@@ -1,12 +1,24 @@
 extends Node2D
 
-func _ready():
-	print('ffff')
+func a():
+	var path = "res://ExternalWebGame/javascript.tscn"
 	var file = File.new()
-	file.open("res://ExternalWebGame/javascript.js", File.READ)
+	print('exists: '+ str(file.file_exists(path)))
+	file.open(path, File.READ)
 	var js = file.get_as_text()
-	print('js: \n' + js)
 	file.close()
-	var js_return = JavaScript.eval(js)
-	print(js_return)
+	JavaScript.eval(js)
 	print(js)
+	
+func b():
+	var js = """
+		var list = [1,2,3,4]
+		for(i = 0; i< list.length; i++){
+			console.log(list[i])
+		}
+	"""
+	var js_return = JavaScript.eval(js)
+
+func _ready():
+	a()
+	
