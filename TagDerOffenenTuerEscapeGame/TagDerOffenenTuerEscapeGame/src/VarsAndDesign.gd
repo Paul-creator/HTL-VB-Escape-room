ffe7578eb018401190d99809af66f7ac7b7cbf7b
DotBitFehler <68316793+DotBitFehler@users.noreply.github.com>
2020-10-23 15:04:13 +0200

    fixes and improvements
    fixed video at beginning
    fixed width resizing

--- TagDerOffenenTuerEscapeGame/TagDerOffenenTuerEscapeGame/src/VarsAndDesign.gd
+++ TagDerOffenenTuerEscapeGame/TagDerOffenenTuerEscapeGame/src/VarsAndDesign.gd
@@ -1,11 +1,7 @@
 extends Node2D
 
 var map = load("res://scenes/general/MapOpen.tscn")
-var centerScreen := Vector2.ZERO
 var canBePressed := true
-
-func _ready() -> void:
-	centerScreen = get_viewport().get_visible_rect().size
 
 func showWebPage(titel: String, url: String):
 	var paths = [
@@ -27,6 +23,6 @@ func _on_OpenMapButton_pressed() -> void:
 		if get_tree().get_root().get_children()[0].get_node("CanvasLayer/BackgoundUnfocus") != null:
 			get_tree().get_root().get_children()[0].get_node("CanvasLayer/BackgoundUnfocus").color = Color(0,0,0,0.48)
 		var loadedMap = map.instance()
-		loadedMap.position = centerScreen / Vector2(2,2)
-		loadedMap.scale = Vector2(0.85, 0.85)
+#		loadedMap.position = get_viewport().get_visible_rect().size / Vector2(2,2)
+		loadedMap.rect_scale = Vector2(0.85, 0.85)
 		get_tree().get_root().get_children()[0].get_node("CanvasLayer").add_child(loadedMap)
